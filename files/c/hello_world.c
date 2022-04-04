e15efbd4a9858d2bdefd4022c3428b4566dbf963
Henry LETELLIER <henry.letellier@epitech.eu>
2022-04-04 23:48:22 +0200

    set up required files for preliminary tests

---
+++ files/c/hello_world.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** github_action_compilation - hello_world.c
+** File description:
+** jitter jitter
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;
+}
